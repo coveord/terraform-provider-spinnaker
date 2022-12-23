736e5099b0fe147b19d296fd3c289709478f7bb5
jheil <jheil@coveo.com>
2022-12-23 12:10:42 +0100

    Make provider work with TF_LOG_PROVIDER

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"log"
+
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/jgramoll/terraform-provider-spinnaker/provider"
 )
 
 func main() {
+	// Terraform expects every line to start with "[LEVEL] " but default value of logger outputs timestamps
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: provider.Provider})
 }
